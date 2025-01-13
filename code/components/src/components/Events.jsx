080039811b5a7f8cfac8a57f8336130f3555ae3c
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-13 14:27:26 -0300

    class 9: rendering functions

--- code/components/src/components/Events.jsx
+++ code/components/src/components/Events.jsx
@@ -3,6 +3,14 @@ const Events = () => {
     const handleMyEvent = () => {
         console.log('Button clicked')
     }
+
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Something</h1>
+        } else {
+            return <h1>Nothing</h1>
+        }
+    }
     
     return (
         <div>
@@ -14,6 +22,10 @@ const Events = () => {
                 <button onClick={() => {
                     console.log('Button clicked 2')
                 }}>Click here too</button>
+            </div>
+            <div>
+                {renderSomething(true)}
+                {renderSomething(false)}
             </div>
         </div>
     )
