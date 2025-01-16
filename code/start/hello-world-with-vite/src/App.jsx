498f03169c87fa3223237a98c58936a66182ee44
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-16 11:32:06 -0300

    feat: envio de teste

--- code/start/hello-world-with-vite/src/App.jsx
+++ code/start/hello-world-with-vite/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <h1>Hello World with Vite</h1>
+      <h1>Hello World</h1>
+      <h2>Tag with style</h2>
     </>
   )
 }
