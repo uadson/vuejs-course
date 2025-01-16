8ff02abadc85261c6f24722a9d7c491b0f09bd23
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-16 10:17:19 -0300

    feat: css de componente

--- code/css/src/App.jsx
+++ code/css/src/App.jsx
@@ -1,10 +1,19 @@
+// Styles
 import './App.css'
+
+// Components
+import CssComponent from './components/CssComponent'
+
 
 function App() {
 
   return (
-    <div className='root'>
+    <div>
       <h1>React with CSS</h1>
+    
+      {/** Css de Component */}
+      <CssComponent />
+      <p>Este parágrafo é do App.jsx</p>
     </div>
   )
 }
---
+++ code/css/src/components/CssComponent.jsx
@@ -0,0 +1,13 @@
+import './CssComponent.css'
+
+const CssComponent = () => {
+  return (
+    <div>
+        <h3>Css Component</h3>
+        <p>Parágrafo do componente, mas seu estilo impactou em App.jsx</p>
+        <p className="css-component">Parágrafo do componente</p>
+    </div>
+  )
+}
+
+export default CssComponent
