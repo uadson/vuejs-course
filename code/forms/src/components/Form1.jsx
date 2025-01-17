56e2f05967f44747398fd302c8c8dc4ff3f7108f
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-17 11:24:28 -0300

    feat: criado primeiro formulário

--- code/forms/src/components/Form1.jsx
+++ code/forms/src/components/Form1.jsx
@@ -1,7 +1,16 @@
+import './Form1.css'
+
 const Form1 = () => {
     return (
-        <>
-        </>
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" name='name' placeholder='Digite o seu nome' />
+                </div>
+                <input type="submit" value="Enviar"/>
+            </form>
+        </div>
     )
 }
 
