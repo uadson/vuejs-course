d2badfd615cd85d817712048084699140a316890
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-17 11:30:33 -0300

    label envolvendo input

--- code/forms/src/components/Form1.jsx
+++ code/forms/src/components/Form1.jsx
@@ -3,11 +3,19 @@ import './Form1.css'
 const Form1 = () => {
     return (
         <div>
+            {/** Formulário simples */}
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
                     <input type="text" name='name' placeholder='Digite o seu nome' />
                 </div>
+                <input type="submit" value="Enviar"/>
+                
+                {/** Label envolvendo input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name='email' placeholder='Digite o seu e-mail'/>
+                </label>
                 <input type="submit" value="Enviar"/>
             </form>
         </div>
