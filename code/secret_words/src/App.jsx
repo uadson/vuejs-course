2cc5d47eb50e8b5924a0519d1c3e88ebdf950e4a
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-21 15:31:33 -0300

    fix #8 feat: Finalizando tela de fim de jogo

--- code/secret_words/src/App.jsx
+++ code/secret_words/src/App.jsx
@@ -122,7 +122,7 @@ function App() {
         score={score}
       />}
 
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'end' && <GameOver retry={retry} score={score}/>}
     </>
   )
 }
