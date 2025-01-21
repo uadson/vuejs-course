895e7f379bdf544db54148b7bb9e0d4b2ea50de9
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-21 11:05:36 -0300

    fix #1 Escolhendo palavra e categoria de forma aleatória

--- code/secret_words/src/App.jsx
+++ code/secret_words/src/App.jsx
@@ -12,7 +12,7 @@ import './App.css'
 //DATA
 import { wordsList } from './data/words'
 
-
+// Game stages
 const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
@@ -22,10 +22,38 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
   
+  const pickedWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * Object.keys(words[category]).length)]
+    
+    return {word, category}
+  }
+
   // Start game
   const startGame = () => {
+    // pick word and category
+    const {word, category} = pickedWordAndCategory()
+    
+    // split word
+    let wordSplit = word.split("")
+    wordSplit = wordSplit.map((letter) => letter.toLowerCase())
+    
+    // fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+    
     setGameStage(stages[1].name)
+    console.log(`Categoria: ${category} - Palavra: ${word}`)
+    console.log(`Word Split: ${wordSplit}`)
   }
 
   // Process input
