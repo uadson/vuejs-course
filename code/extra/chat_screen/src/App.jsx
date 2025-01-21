d876c520d62152a1fba1d3b98e0116b9f9eef7a6
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-21 17:10:03 -0300

    feat: definição da tela de chat

---
+++ code/extra/chat_screen/src/App.jsx
@@ -0,0 +1,15 @@
+// CSS
+import './App.css'
+
+// Components
+import ChatScreen from './components/ChatScreen'
+
+function App() {
+  return (
+    <>
+    <ChatScreen />
+    </>
+  )
+}
+
+export default App
