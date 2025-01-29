d4c7b041cbf510917bcb199e1cf3297248d45d11
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2025-01-29 14:31:09 -0300

    feat: criado rota para listar produtos

--- code/http_requests/backend/src/models.py
+++ code/http_requests/backend/src/models.py
@@ -1,3 +1,4 @@
+from pydantic import field_validator
 from sqlmodel import Field, SQLModel
 
 
@@ -5,6 +6,13 @@ class ProductBase(SQLModel):
     name: str = Field(index=True)
     price: float = Field()
     
+    @field_validator('price')
+    def price_validate(cls, value):
+        if value < 0:
+            raise ValueError(f"Valor informado '{value}' é inválido (Valor Negativo).")
+        else:
+            return float(value)
+        
 
 class Product(ProductBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
--- code/http_requests/backend/src/routes.py
+++ code/http_requests/backend/src/routes.py
@@ -1,15 +1,31 @@
 from db import get_session
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Query
 from models import ProductPublic, ProductCreate, Product
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
 
 router = APIRouter()
 
 
 @router.post('/', response_model=ProductPublic, status_code=status.HTTP_201_CREATED)
-async def create(*, session: AsyncSession = Depends(get_session), product:ProductCreate):
+async def create_product(*, session: AsyncSession = Depends(get_session), product:ProductCreate):
     db_product = Product.model_validate(product)
     session.add(db_product)
     await session.commit()
     session.refresh(db_product)
     return db_product
+
+
+@router.get('/', response_model=list[ProductPublic], status_code=status.HTTP_200_OK)
+async def read_products(
+    *, 
+    session: AsyncSession = Depends(get_session),
+    offset: int=0,
+    limit: int = Query(default=100, le=100)
+):
+    async with session as session_db:
+        query = select(Product).offset(offset).limit(limit)
+        result = await session.execute(query)
+        products: list[ProductPublic] = result.scalars().all()
+        
+        return products
