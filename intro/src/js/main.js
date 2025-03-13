0f3fb9c768447bc1d0cfc1bbf96268b42f8b5841
Uadson Feitosa <uadsonpy@gmail.com>
2025-03-13 14:36:08 -0300

    v-bind:class

---
+++ intro/src/js/main.js
@@ -0,0 +1,77 @@
+var productsList = [
+    {
+        "photo": "/src/assets/big-mac.png",
+        "name": "Big Mac",
+        "price": 5.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/mc-chicken.png",
+        "name": "Mc Chicken",
+        "price": 4.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/double-cb.png",
+        "name": "Double Cheese Burger",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/fries.png",
+        "name": "Batata frita",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/nuggets.png",
+        "name": "Mc Nuggets",
+        "price": 3.49,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/salad.png",
+        "name": "Salada",
+        "price": 2.79,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/cola.png",
+        "name": "Coca Cola",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/lipton.png",
+        "name": "Ice Tea",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "/src/assets/water.png",
+        "name": "Água",
+        "price": 1.49,
+        "active": false,
+        "quantity": 1
+    }
+];
+
+
+
+const productsService = {
+    data() {
+        return {
+            products: window.productsList
+        }
+    }
+}
+
+Vue.createApp(productsService).mount("#app")
